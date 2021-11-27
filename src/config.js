784e1480e535b344c0dbd2da976caf4016527252
zoran.liu <zoran.liu@ambergroup.io>
2021-11-27 17:21:25 +0800

    fix: update config

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
+let prefix = 'redisgo/v3'
 const wsProtocol = location.protocol === 'http:' ? 'ws:' : 'wss:'
-let local = '127.0.0.1:59299/redisgo/v3'
+let local = `127.0.0.1:59299/${prefix}`
 
 let config = {
     'local': { base_url: `${location.protocol}//${local}`, ws_url: `${wsProtocol}//${local}/ws`},
-    'prod': { base_url: `${location.origin}/redisgo/v3`, ws_url: `${wsProtocol}//${location.host}/redisgo/v3/ws`},
+    'prod': { base_url: `${location.origin}/${prefix}`, ws_url: `${wsProtocol}//${location.host}/${prefix}/ws`},
 }
 
-let env = 'local'
-// let env = 'dev'
-// let env = 'prod'
+// let env = 'local'
+let env = 'prod'
 export default {
     base_url: config[env].base_url,
     ws_url: config[env].ws_url,
